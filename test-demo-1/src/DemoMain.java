ed7712f0b4b779ad8d8430eab67dd1ebe23d8feb
chenchen2016-a <985590680@qq.com>
2020-12-04 22:56:04 +0800

    尝试进行第2次增改

--- test-demo-1/src/DemoMain.java
+++ test-demo-1/src/DemoMain.java
@@ -1,6 +1,9 @@
+import service.CalculationService;
+
 public class DemoMain {
     public static void main(String[] args) {
         System.out.println("Hello World ！！ ");
+        System.out.println(new CalculationService().add(1.2,3.5));
     }
 }
 
---
+++ test-demo-1/src/service/CalculationService.java
@@ -0,0 +1,7 @@
+package service;
+
+public class CalculationService {
+    public double add(double a, double b) {
+        return a+b;
+    }
+}
